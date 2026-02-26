860c75a45c8e511f3f9e8ff37e89128a1cdc4cae
Gyanendra876 <harshsinha06z@gmail.com>
2026-02-26 07:38:38 +0530

    Update socket connection URL to new server

--- client/src/services/socket.js
+++ client/src/services/socket.js
@@ -1,5 +1,5 @@
 import { io } from "socket.io-client";
 
-export const socket = io("https://connectmeet-backend.onrender.com", {
+export const socket = io("https://connectmeet-server.onrender.com", {
   autoConnect: false,
 });
